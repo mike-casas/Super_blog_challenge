d372613a771dea2d38e22a0aaaa71e8fd1def187
Miguel Casas <miguel@kommit.co>
2016-04-10 17:05:06 -0500

    add ajax to views reply

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -1,24 +1,12 @@
 class RepliesController < ApplicationController
  before_action :authenticate_user!
-  def index
-
-  end
-
-  def new
-
-  end
-  def show
-  end
   def create
+    @reply_description= params_reply[:description]
+    @reply_owner= params_reply[:user][:name]
     @coment= Coment.find(params[:coment_id])
     @coment.replies.create(params_reply)
-
-    redirect_to request.referrer || root_url
+    # redirect_to request.referrer || root_url
   end
-  def destroy
-
-  end
-
   private
   def params_reply
     params.require(:reply).permit(:description)
